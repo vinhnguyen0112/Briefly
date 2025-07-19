f7ade98e929860ab3a46177e19a3d04fa4e80958
capstone_2025_1 <capstone_2025_1@coccoc.com>
2025-07-19 09:50:49 +0700

    Fix: Add database fallback

--- server/helpers/dbHelper.js
+++ server/helpers/dbHelper.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  database: process.env.MYSQL_DATABASE || "capstone",
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
