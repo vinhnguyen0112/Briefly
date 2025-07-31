69381243e039d6930e6a30cc12966d46ccb3d0bc
hoangdv <duongviethoang2042002@gmail.com>
2025-07-31 11:22:44 +0700

    add tag button

--- extension/scripts/components/notes-handler.js
+++ extension/scripts/components/notes-handler.js
@@ -426,13 +426,17 @@ function createNoteItem(note, showUrl = false) {
     </div>
     ${urlDisplay}
     <div class="note-actions">
-      <button class="note-action-button edit-note" title="Edit Note">
+      <button class="note-action-button edit-note" 
+              data-i18n-title="editNote" 
+              title="${translate("editNote")}">
         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
           <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
         </svg>
       </button>
-      <button class="note-action-button delete-note" title="Delete Note">
+      <button class="note-action-button delete-note" 
+              data-i18n-title="deleteNote" 
+              title="${translate("deleteNote")}">
         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <polyline points="3 6 5 6 21 6"></polyline>
           <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
